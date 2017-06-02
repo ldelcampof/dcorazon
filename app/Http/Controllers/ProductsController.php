e250fd265fc908be90653e97f52444a6a361b6c0
Luis Javier del Campo Flores <l.delcampof@gmail.com>
2017-06-02 11:22:47 -0500

    Guardando formulario con archivos

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -39,110 +39,17 @@ class ProductsController extends Controller
     public function store(Request $request)
     {
         $data = $request->all();
-
-        Validator::make($data, Product::getRules(), Product::getMessages())->validate();
-
-        $product = Product::create($data);
-
-        return $return;
-
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-}
-
-
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use App\Models\Product;
-use Validator;
-
-class ProductsController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $products = Product::paginate(20);
-
-        return $products;
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $data = $request->all();
         $dataProduct = (array) json_decode($data['product']);
-
-        dd($dataProduct);
 
         Validator::make($dataProduct, Product::getRules(), Product::getMessages())->validate();
 
-        $product = Product::create($data);
+        $destinationPath = '/public/uploads/productos';
+        $file = $request->filefoto->store($destinationPath);
+        $path = str_replace('public/', 'storage/', $file);
+
+        $product = new Product($dataProduct);
+        $product->foto = $path;
+        $product->save();
 
         return $product;
 
